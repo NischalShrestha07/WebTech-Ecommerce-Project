8a492f4c277a41a6ab3eed320a772fecbdb85066
Nischal <nischalshrestha800@gmail.com>
2025-03-20 23:05:23 +0545

    Layouts Components

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,24 @@
-<x-app-layout>
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+</head>
 
+<body>
+    <div class="container-scroller">
+        @include('admin.navbar')
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+        <!-- partial -->
+        @include('admin.body')
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+    <!-- End custom js for this page -->
+</body>
 
-<h1>Welcome to Admin Dashboard</h1>
+</html>
